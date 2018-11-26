032233761fba5404a7de9a169cd42e1d2b9fd166
Mikael Karon <mikael@karon.se>
2018-11-26 23:23:26 +0100

    feat: Added json file serialization to plugin-git

--- plugins/plugin-git/src/commands/json.js
+++ plugins/plugin-git/src/commands/json.js
@@ -1,21 +1,27 @@
+const {createWriteStream} = require('fs')
 const BaseCommand = require('../lib/command')
 const {MemRepo, FsRepo} = require('../lib/repo')
 
 class JsonCommand extends BaseCommand {
-  async list(repo, url, options) {
-    let count = 0
-    for await (const entry of repo.listEntries(await this.tree(repo, url, options), options)) {
-      this.log(JSON.stringify(entry))
-      count++
+  async list(repo, url, target, options) {
+    const log = target ? message => target.write(`${message}\n`) : this.log
+    try {
+      let count = 0
+      for await (const entry of repo.listEntries(await this.tree(repo, url, options), options)) {
+        log(JSON.stringify(entry))
+        count++
+      }
+      this.log(`Listed ${count} ${count === 1 ? 'entry' : 'entries'}`)
+    } finally {
+      target.end()
     }
-    this.log(`Listed ${count} ${count === 1 ? 'entry' : 'entries'}`)
   }
 
   async run() {
     const {args, flags} = this.parse(JsonCommand)
-    const {source} = args
+    const {source, target} = args
     try {
-      await this.list(source.protocol ? new MemRepo() : new FsRepo(source.path), source, flags)
+      await this.list(source.protocol ? new MemRepo() : new FsRepo(source.path), source, target, flags)
     } catch (error) {
       this.error(error.message, {exit: 1})
     }
@@ -23,7 +29,14 @@ class JsonCommand extends BaseCommand {
 }
 
 JsonCommand.description = 'lists entries in a BlockFor.fun git registry.'
-JsonCommand.args = BaseCommand.args
+JsonCommand.args = [
+  ...BaseCommand.args,
+  {
+    name: 'target',
+    description: 'path to target json file',
+    parse: input => createWriteStream(input),
+  },
+]
 JsonCommand.flags = BaseCommand.flags
 
 module.exports = JsonCommand
